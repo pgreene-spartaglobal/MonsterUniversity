ee107de20ff8141386eb0ca12363578f5d3c1ae9
Philip Greene <PGreene@spartaglobal.com>
2019-09-02 11:19:37 +0100

    Implemented file stream to read monsters from a .txt file

--- MonsterUniversity/Program.cs
+++ MonsterUniversity/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MonsterUniversity
 {
@@ -15,15 +16,31 @@ namespace MonsterUniversity
 
             MonsterCohort monsterCohort = new MonsterCohort();
             monsterCohort.AddStudent(defaultMonster);
+            
+            StreamReader file =  new StreamReader("monster_students.txt");
+            Console.WriteLine(Directory.GetCurrentDirectory());
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] words = line.Split(',');
+                monsterCohort.listOfStudents.Add(new MonsterStudent(Convert.ToInt32(words[0]), words[1], Convert.ToInt32(words[2]), words[3], Convert.ToDouble(words[4]), words[5]));
+            }
+
+            file.Close();
+
             monsterCohort.ListStudents();
         }
+    
     }
 
     class Monster
     {
         public int id;
+        public string name;
         public int eyes;
-        public string hairColour;
+        public string colour;
         public double scareLevel;
         public string specialAbility;
 
@@ -43,10 +60,20 @@ namespace MonsterUniversity
         public MonsterStudent()
         {
             this.id = 0;
+            this.name = "DefaultName";
             this.eyes = 2;
-            this.hairColour = "blue";
+            this.colour = "DefaultColour";
             this.scareLevel = 50;
-            this.specialAbility = "Roar!";
+            this.specialAbility = "DefaultSpecialAbility";
+        }
+
+        public MonsterStudent(int id, string name, int eyes, string colour, double scareLevel, string specialAbility)
+        {
+            this.id = id;
+            this.name = name;
+            this.colour = colour;
+            this.scareLevel = scareLevel;
+            this.specialAbility = specialAbility;
         }
     }
 
@@ -54,7 +81,7 @@ namespace MonsterUniversity
     {
         string subject;
         DateTime startDate;
-        List<MonsterStudent> listOfStudents = new List<MonsterStudent>();
+        public List<MonsterStudent> listOfStudents = new List<MonsterStudent>();
 
         public MonsterCohort()
         {
@@ -71,7 +98,7 @@ namespace MonsterUniversity
         {
             foreach (MonsterStudent monsterStudent in listOfStudents)
             {
-                Console.WriteLine(String.Format("ID: {0}\nEyes: {1}\nHair Colour: {2}\nScare Level: {3}\nSpecial Ability: {4}",monsterStudent.id, monsterStudent.eyes, monsterStudent.hairColour, monsterStudent.scareLevel, monsterStudent.scareLevel));
+                Console.WriteLine(String.Format("\nID: {0}\nEyes: {1}\nHair Colour: {2}\nScare Level: {3}\nSpecial Ability: {4}",monsterStudent.id, monsterStudent.eyes, monsterStudent.colour, monsterStudent.scareLevel, monsterStudent.scareLevel));
             }
         }
     }
